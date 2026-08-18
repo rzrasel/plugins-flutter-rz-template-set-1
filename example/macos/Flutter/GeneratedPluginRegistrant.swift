65d80e9794271377b7d2632c000cef1abaffc0d8
Rz Rasel <emailrzrasel@gmail.com>
2026-08-19 00:43:01 +0600

    Initial Commit - 2026-08-19 00:43:00

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,12 +9,10 @@ import responsive_builder_kit
 import rz_library_utilities
 import rz_template_set_1
 import rz_theme_set_1
-import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ResponsiveBuilderKitPlugin.register(with: registry.registrar(forPlugin: "ResponsiveBuilderKitPlugin"))
   RzLibraryUtilitiesPlugin.register(with: registry.registrar(forPlugin: "RzLibraryUtilitiesPlugin"))
   RzTemplateSet_1Plugin.register(with: registry.registrar(forPlugin: "RzTemplateSet_1Plugin"))
   RzThemeSet_1Plugin.register(with: registry.registrar(forPlugin: "RzThemeSet_1Plugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
